97aa7e7d1244abfff314d1323fa33e486f4debcc
aloke <alokekumarsingh94@gmail.com>
2020-08-03 01:04:03 +0530

    Final Changes

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ This is a fun game created using pure vanilla JavaScript.
 GAME RULES:
 
 - The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- In each turn, a player rolls TWO dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1 in any one of them , all his ROUND score gets lost. After that, it's the next player's turn
+- If a player rolls a 6 in both the disc at the same time then active player looses all his GLOBAL score.
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
+- An input field is there where players can set the winning score, so that they can change the predefined score of 100.
+- The first player to reach 100 points or winning score set by player wins the game
--- roll-a-dice-game/app.js
+++ roll-a-dice-game/app.js
@@ -2,10 +2,12 @@
 GAME RULES:
 
 - The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
+- In each turn, a player rolls TWO dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1 in any one of them , all his ROUND score gets lost. After that, it's the next player's turn
+- If a player rolls a 6 in both the disc at the same time then active player looses all his GLOBAL score.
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
+- An input field is there where players can set the winning score, so that they can change the predefined score of 100.
+- The first player to reach 100 points or winning score set by player wins the game
 
 */
 
@@ -17,14 +19,23 @@ init();
 document.querySelector('.btn-roll').addEventListener('click', function () {
 
    if (gamePlaying) {
-      var dice = Math.floor(Math.random() * 6) + 1;
-      var diceDOM = document.querySelector('.dice');
-      diceDOM.style.display = 'block';
-      diceDOM.src = 'dice-' + dice + '.png';
+      var dice1 = Math.floor(Math.random() * 6) + 1;
+      var dice2 = Math.floor(Math.random() * 6) + 1;
+      document.getElementById("dice1").style.display = 'block';
+      document.getElementById("dice2").style.display = 'block';
 
-      if (dice !== 1) {
-         roundScore += dice;
-         document.querySelector('#current-' + activePlayer).textContent = roundScore;
+      document.getElementById("dice1").src = 'dice-' + dice1 + '.png';
+      document.getElementById("dice2").src = 'dice-' + dice2 + '.png';
+
+      if (dice1 !== 1 && dice2 !== 1) {
+         if (dice1 === 6 && dice2 === 6) {
+            scores[activePlayer] = 0;
+            document.querySelector('#score-' + activePlayer).textContent = '0';
+            nextPlayer();
+         } else {
+            roundScore += dice1 + dice2;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+         }
       } else {
          nextPlayer();
 
@@ -42,10 +53,19 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
 
       document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-      if (scores[activePlayer] >= 100) {
+      var input = document.getElementById("myFinalScore").value;
+      var winningScore;
+      if (input) {
+         winningScore = input;
+      } else {
+         winningScore = 100;
+      }
+
+      if (scores[activePlayer] >= winningScore) {
 
          document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
-         document.querySelector('.dice').style.display = 'none';
+         document.getElementById("dice1").style.display = 'none';
+         document.getElementById("dice2").style.display = 'none';
          document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
          document.querySelector('.player-' + activePlayer + '-panel').classList.add('active');
          gamePlaying = false;
@@ -72,7 +92,8 @@ function nextPlayer() {
    document.querySelector('.player-0-panel').classList.toggle('active');
    document.querySelector('.player-1-panel').classList.toggle('active');
 
-   document.querySelector('.dice').style.display = 'none';
+   document.getElementById("dice1").style.display = 'none';
+   document.getElementById("dice2").style.display = 'none';
 }
 
 
@@ -84,7 +105,8 @@ function init() {
    activePlayer = 0;
    roundScore = 0;
 
-   document.querySelector('.dice').style.display = 'none';
+   document.getElementById("dice1").style.display = 'none';
+   document.getElementById("dice2").style.display = 'none';
 
    document.getElementById('score-0').textContent = '0';
    document.getElementById('score-1').textContent = '0';
@@ -97,5 +119,8 @@ function init() {
    document.querySelector('.player-0-panel').classList.remove('active');
    document.querySelector('.player-1-panel').classList.remove('active');
    document.querySelector('.player-0-panel').classList.add('active');
-   gamePlaying=true;
+   gamePlaying = true;
+   if (document.getElementById('myFinalScore').value) {
+      document.getElementById('myFinalScore').value = '';
+   }
 }
